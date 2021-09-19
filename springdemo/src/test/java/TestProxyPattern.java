ce4e82a240bda5b2d4293edcf5aeb8f92f58cdb6
491919550@qq.com <491919550@qq.com>
2021-09-19 23:56:38 +0800

    test

--- springdemo/src/test/java/TestProxyPattern.java
+++ springdemo/src/test/java/TestProxyPattern.java
@@ -6,7 +6,6 @@ public class TestProxyPattern {
     @Test
     public  void test() {
         Image image = new ProxyImage("test_10mb.jpg");
-
         // 图像将从磁盘加载
         image.display();
         System.out.println("");
